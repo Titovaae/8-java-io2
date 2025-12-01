f497fcb4b3face4cdbf751cf1cf4010b61d5e69a
titovaae <miya.dark@bk.ru>
2025-12-02 00:54:05 +0500

    ываыв

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,9 @@
 package com.example.task01;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Task01Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -15,6 +17,21 @@ public class Task01Main {
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
         // your implementation here
+        String executableFileName = "C:\\Users\\User\\Desktop\\ffmpeg-2025-12-01-git-7043522fe0-full_build\\bin\\ffprobe.exe";
+        String mp3FileName = file.getPath();
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command(executableFileName, "-v", "error", "-of", "flat", "-show_format", mp3FileName);
+
+        Process process = pb.start();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if(line.startsWith("format.tags.title")){
+                    return line.split("=")[1].replace("\"", "");
+                }
+            }
+        }
         return "sound name";
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,42 @@
 package com.example.task02;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.channels.*;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
+
         System.out.println(listFiles(Paths.get("task02/src/main/resources/")));
-        */
+
 
     }
 
     public static List<Path> listFiles(Path rootDir) throws IOException, InterruptedException {
         // your implementation here
-
-        return null;
+        List<Path> files = new ArrayList<>();
+        Stack<Path> stack = new Stack<>();
+        stack.push(rootDir);
+        while (!stack.isEmpty()) {
+            Path path = stack.pop();
+            if (path.toFile().isFile()) {
+                files.add(path);
+            }
+            else  {
+                for (File p : path.toFile().listFiles()) {
+                    stack.add(p.toPath());
+                }
+            }
+        }
+        return files;
     }
 }
